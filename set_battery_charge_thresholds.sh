dbafccdfeac0712c3765320fdf13e687db7fe5b6
John Cabaj <johncabaj@outlook.com>
2023-04-12 16:57:53 -0500

    Updating comments on usage for setting battery thresholds

--- set_battery_charge_thresholds.sh
+++ set_battery_charge_thresholds.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+# 
+# NOTE: Requires systemd .service file similar to below and enabled to use
+#
+# Description = Setting battery charge thresholds
+# 
+# [Service]
+# Type=simple
+# ExecStart=/usr/bin/bash -c /home/john/Scripts/set_battery_charge_thresholds.sh
+# 
+# [Install]
+# WantedBy=multi-user.target
 
 # set battery charge start threshold to 75%
 echo 75 > /sys/class/power_supply/BAT0/charge_start_threshold
